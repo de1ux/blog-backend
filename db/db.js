79155bf58ad3551fac459f52013061184cf9950a
Nathan Evans <nathan@de1ux.com>
2022-08-17 16:50:20 -0600

    Make the database work deployed

--- db/db.js
+++ db/db.js
@@ -1,9 +1,29 @@
 //#3 setup DB models
 const Sequelize = require("sequelize");
 
-const db = new Sequelize("postgres://hackupstate@localhost:5432/blog", {
-	logging: false,
-});
+
+let options = {};
+let databaseURL = process.env.DATABASE_URL;
+if (!databaseURL) {
+	// this means we're on localhost!
+	databaseURL = "postgres://hackupstate@localhost:5432/blog";
+	options = {
+		logging: false,
+	};
+} else {
+	// we're not on localhost
+	options = {
+		logging: false,
+		dialectOptions: {
+			ssl: {
+				require: true,
+				rejectUnauthorized: false
+			}
+		}
+	};
+}
+
+const db = new Sequelize(databaseURL, options);
 const User = require("./User")(db);
 const Post = require("./Post")(db);
 
